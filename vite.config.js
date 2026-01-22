b4cbf917c1c2b68eaa3eb65c4f3960f637051603
Sabin Shrestha <162944408+sabinshrestha1@users.noreply.github.com>
2026-01-22 17:10:21 +0545

    Update base path in vite.config.js
    Change base path for asset linking in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked relatively, helping with file:// protocol or subpath deployments
+  base: "/crowndistilleryreact/",
 })
